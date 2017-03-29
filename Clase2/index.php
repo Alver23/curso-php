2e9842431d9c46a988f912bb094224702f5a492e
Alver Alexander Grisales Ortega <viga.23@hotmail.com>
2017-03-28 23:12:02 -0500

    Fin clase 2

--- Clase2/index.php
+++ Clase2/index.php
@@ -1,7 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: agrisales
- * Date: 28/03/17
- * Time: 10:28 PM
- */
+for ($x = 50; $x >= 0; $x-= 10) {
+	
+	if ($x == 30) {
+		print "entre en condicion 30 <br>";
+	} else if ($x == 20) {
+		print "entre en condicion 20 <br>";
+	} else {
+		print $x ."<br>";
+	}
+}
+$y = 0;
+print "<br>";
+while ($y <= 10) {
+	switch ($y) {
+		case 6:
+			print "print caso 6";
+			break;
+		case 10:
+			print "caso 10 termine";
+		break;
+		default:
+			print $y ."<br>";
+			break;
+	}
+	$y = $y + 2;
+}
+
+$z = 0;
+print "<br>";
+do {
+	print $z ."<br>";
+	$z = $z + 3;
+} while ($z <= 21);
